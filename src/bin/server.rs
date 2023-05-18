ff93c94e5a35c22da61708051677518a7b5351cb
vidur2 <vmod2005@gmail.com>
2023-05-18 13:52:45 -0400

    thing

--- src/bin/server.rs
+++ src/bin/server.rs
@@ -1,3 +1,18 @@
-fn main() {
-    
+use std::sync::{Mutex, Arc};
+
+use actix_web::{HttpServer, App};
+use easy_nas_rs::{file_handler::file_store::FileStore, server::{edit_file, read_file}};
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    let file_store = Arc::new(Mutex::new(FileStore::new()));
+
+    HttpServer::new(move || {
+        App::new()
+        .app_data(Arc::clone(&file_store))
+        .service(edit_file)
+        .service(read_file)
+    }).bind(("0.0.0.0", 5001))?
+    .run()
+    .await
 }
